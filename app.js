4b5e2fb9bd789e6d5338b39efa7facb59f45886a
Zero <mlacayo00@ccny.cuny.edu>
2014-11-14 23:10:28 -0500

    changed

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 (function(){
   var app = angular.module('bulletinapp', []);
   
+  var tabs =app.controller('tabs',function(){
+	this.tab=1;
+	
+	this.setTab = function(t){
+		this.tab=t;
+	};
+	
+	this.isSelected = function(tab){
+		tab===this.tab;
+	};
+  });
   var login = app.controller('login', function(){
     this.bar = false;
     this.toggle = function(){
